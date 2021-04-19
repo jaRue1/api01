85197e01d2f821a79b33cc080df12bdfb5a96859
jaRue1 <jaruejohnson@outlook.com>
2021-04-19 17:58:07 -0400

    added some more examples

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { request } = require("express")
 const express = require("express")
-const { getCandidates, getSingleCandidate ,getSingleCandidates, getFavoriteColor} = require('./src/candidates')
+const { getCandidates, getSingleCandidate ,getSingleCandidates, getFavoriteColor, getAlphabeticalOrder, getCandidatesHobbies} = require('./src/candidates')
 const port = 3000
 const app = express()
 
-app.get("/candidate/color/:favColor", getFavoriteColor)
+app.get("/candidates/hobbies/", getCandidatesHobbies)
+app.get("/candidates/ordered/", getAlphabeticalOrder)
+app.get("/candidate/color/:myColor", getFavoriteColor)
 app.get("/candidate/id/:candidateId", getSingleCandidates)
 app.get("/candidate", getSingleCandidate)
 app.get("/candidates", getCandidates)
--- src/candidates/index.js
+++ src/candidates/index.js
@@ -2,31 +2,37 @@
   const candidates = [
     {
       name: "Vitoria Roas",
+      classId : 0001,
       favColor: "blue",
       hobby: " painting ",
     },
     {
       name: "Jonah Allen",
+      classId : 0002,
       favColor: "blue",
       hobby: "Sleeping ",
     },
     {
       name: "Kevin Choi",
+      classId : 0003,
       favColor: "blue",
       hobby: "reading ",
     },
     {
       name: "Jarue Johnson",
+      classId : 0004,
       favColor: "blue",
       hobby: " Making Beats",
     },
     {
       name: "Nikki Allen",
+      classId : 0005,
       favColor: "purple",
       hobby: "planting",
     },
     {
       name: "Adrian Pedromo",
+      classId : 0006,
       favColor: "blue",
       hobby: "Cooking",
     }]
@@ -62,11 +68,42 @@
     }
     exports.getFavoriteColor = (request, response) => {
       console.log ({params : request.params})
-      const {favColor} = request.params
-      const myCandidates = candidates.filter(candidate => candidate.favColor === favColor)
+      const {myColor} = request.params
+      const myCandidates = candidates.filter(candidate => candidate.favColor === myColor)
       response.status(200).json({
         status: 200,
         data: myCandidates,
         message : " Show's the color "
+      })
+    }
+    exports.getAlphabeticalOrder = (request, response) => {
+      console.log ({params : request.params})
+      const sortedCandidates = candidates.sort((x,y) => {
+        let first = x.name.toLowerCase()
+        let last = y.name.toLowerCase()
+        if (first < last) {
+          return -1
+        }
+        if (first > last) {
+          return 1
+        }
+        return 0
+      })
+      response.status(200).json({
+        status: 200,
+        data: sortedCandidates,
+        message : " Sorts the candidates "
+      })
+    }
+    exports.getCandidatesHobbies = (request, response) => {
+      const candidateHobbies = []
+      for (let i = 0; i < candidates.length; i++) {
+        const element = candidates[i].hobby;
+        candidateHobbies.push(element)
+      }
+      response.status(200).json({
+        status: 200,
+        data: candidateHobbies,
+        message : "Gets the candidates hobbies"
       })
     }
